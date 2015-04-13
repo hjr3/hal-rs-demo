813fb6813cbf1f25824b1a4f43de1539bf9cd299
Herman J. Radtke III <hermanradtke@gmail.com>
2015-04-13 16:30:35 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 #![feature(phase)]
 
-extern crate http;
 extern crate nickel;
 extern crate hal;
-extern crate serialize;
+extern crate rustc_serialize as serialize;
 extern crate postgres;
 extern crate time;
-#[phase(plugin)] extern crate nickel_macros;
+#[macro_use] extern crate nickel_macros;
 
-use std::io::net::ip::Ipv4Addr;
-use nickel::{Nickel, Request, Response, HttpRouter, Continue, Halt, MiddlewareResult};
-use nickel::{NickelError, ErrorWithStatusCode, mimes};
+use nickel::{Nickel, Request, Response, HttpRouter, Continue, Halt, Action};
+use nickel::{NickelError, mimes};
 use hal::{Link, Resource, ToHal};
 use serialize::json::{ToJson, Json};
-use postgres::{Connection, NoSsl, Row, Rows, Statement, ToSql, ResultDescription};
+use postgres::{Connection, SslMode, Row, Rows, Statement, ToSql, Column};
 use postgres::types::{Type};
-use std::os;
-use http::status::{NotFound, BadRequest};
+use std::{os, env};
+use std::str::FromStr;
+use nickel::status::StatusCode::{NotFound, BadRequest};
 
 struct Order {
     order_id: i32,
@@ -35,9 +34,9 @@ impl ToHal for Order {
 }
 
 fn get_option(key: &str, default: &str) -> String {
-    match os::getenv(key) {
-        Some(val) => val,
-        None => String::from_str(default)
+    match env::var(key) {
+        Ok(val) => val.parse().unwrap(),
+        Err(..) => default.to_string()
     }
 }
 
@@ -49,78 +48,72 @@ fn connect() -> Connection {
     let dbname = get_option("DBNAME", "myapp");
 
     let dsn = format!("postgres://{}:{}@{}:{}/{}", user, password, host, port, dbname);
-    Connection::connect(dsn.as_slice(), &NoSsl).unwrap()
+    Connection::connect(dsn.as_slice(), &SslMode::None).unwrap()
 }
 
-fn not_found_handler(message: String, response: &mut Response) {
-    let error = Resource::new()
-        .add_state("message", message)
-        .add_link("help", Link::new("/help"));
-
-    // todo: add vnd.error profile
-    response
-        .status_code(NotFound)
-        .content_type(mimes::Hal)
-        .send(format!("{}", error.to_json())); 
-}
-
-fn bad_request_handler(message: String, response: &mut Response) {
-    let error = Resource::new()
-        .add_state("message", message)
-        .add_link("help", Link::new("/help"));
-
-    // todo: add vnd.error profile
-    response
-        .status_code(BadRequest)
-        .content_type(mimes::Hal)
-        .send(format!("{}", error.to_json())); 
-}
-
-fn error_handler(err: &NickelError, _request: &Request, response: &mut Response) -> MiddlewareResult {
-    let message = err.message.to_string();
-
-    match err.kind {
-        ErrorWithStatusCode(BadRequest) => {
-            bad_request_handler(message, response);
-            Ok(Halt)
-        },
-        ErrorWithStatusCode(NotFound) => {
-            not_found_handler(message, response);
-            Ok(Halt)
-        },
-        _ => Ok(Continue)
-    }
-}
-
-fn logger(request: &Request, _response: &mut Response) -> MiddlewareResult {
-    let date_time = time::now();
-
-    println!("{} {}", date_time.rfc3339(), request.origin.request_uri);
-
-    Ok(Continue)
-}
+// error handling is not working in nickel at the moment
+//
+//fn not_found_handler(message: String, response: &mut Response) {
+//    let error = Resource::new()
+//        .add_state("message", message)
+//        .add_link("help", Link::new("/help"));
+//
+//    // todo: add vnd.error profile
+//    response
+//        .content_type(mimes::MediaType::Hal)
+//        .send(format!("{}", error.to_json())); 
+//}
+//
+//fn bad_request_handler(message: String, response: &mut Response) {
+//    let error = Resource::new()
+//        .add_state("message", message)
+//        .add_link("help", Link::new("/help"));
+//
+//    // todo: add vnd.error profile
+//    response
+//        .content_type(mimes::MediaType::Hal)
+//        .send(format!("{}", error.to_json())); 
+//}
+//
+//fn error_handler<'a>(err: &mut NickelError, _request: &mut Request) -> Action {
+//    let message = err.message.to_string();
+//
+//    if let Some(ref mut response) = err.stream {
+//        match response.status() {
+//            BadRequest => {
+//                bad_request_handler(message, response);
+//                return Halt(())
+//            },
+//            NotFound => {
+//                not_found_handler(message, response);
+//                return Halt(())
+//            },
+//            _ => Continue(())
+//        }
+//    }
+//}
 
 // todo: consider returning a Result<> instead of defaulting to Null
-fn pgsql_to_hal(descs: &[ResultDescription], row: &Row) -> Resource {
+fn pgsql_to_hal(descs: &[Column], row: &Row) -> Resource {
     let mut hal = Resource::new();
     for desc in descs.iter() {
-        let column_name = desc.name.as_slice();
+        let column_name = desc.name().as_slice();
 
-        match desc.ty {
-            Type::Varchar => {
+        match desc.type_() {
+            &Type::Varchar => {
                 let value: String = row.get(column_name);
                 hal = hal.add_state(column_name, value);
             },
-            Type::Int4 => {
+            &Type::Int4 => {
                 let value: i32 = row.get(column_name);
                 hal = hal.add_state(column_name, value as i64);
             },
-            Type::Float8 => {
+            &Type::Float8 => {
                 let value: f64 = row.get(column_name);
                 hal = hal.add_state(column_name, value);
             },
             _ => {
-                println!("type is: {}", desc.ty);
+                println!("type is: {:?}", desc.type_());
                 hal = hal.add_state(column_name, ());
             }
         }
@@ -132,7 +125,10 @@ fn pgsql_to_hal(descs: &[ResultDescription], row: &Row) -> Resource {
 fn main() {
 
     let mut server = Nickel::new();
-    server.utilize(logger);
+    server.utilize(middleware! { |request|
+        let date_time = time::now();
+        println!("{} logging request: {:?}", date_time.rfc3339(), request.origin.uri);
+    });
 
     server.utilize(router! {
         get "/" => |request, response| {
@@ -142,8 +138,8 @@ fn main() {
                 .add_link("ea:find", Link::new("/orders{?id}").templated(true))
                 .add_link("ea:admin", Link::new("/admins/2").title("Fred"))
                 .add_link("ea:admin", Link::new("/admins/5").title("Kate"))
-                .add_state("currentlyProcessing", (14 as int))
-                .add_state("shippedToday", (14 as int))
+                .add_state("currentlyProcessing", (14 as i64))
+                .add_state("shippedToday", (14 as i64))
                 .add_resource("ea:order",
                     Resource::with_self("/orders/123")
                         .add_link("ea:basket", Link::new("/baskets/98712"))
@@ -163,16 +159,16 @@ fn main() {
 
             let results = orders.to_json();
             response
-                .content_type(mimes::Hal)
+                .content_type(mimes::MediaType::Hal)
                 .send(format!("{}", results)); 
         }
 
         get "/orders/:order_id" => |request, response| {
             let conn = connect();
 
-            let order_id: i32 = match from_str(request.param("order_id")) {
-                Some(order_id) => order_id,
-                None => return Err(NickelError::new("Invalid order id", ErrorWithStatusCode(BadRequest)))
+            let order_id: i32 = match FromStr::from_str(request.param("order_id")) {
+                Ok(order_id) => order_id,
+                Err(..) => return Err(NickelError::new(response, "Invalid order id", BadRequest))
             };
 
             let stmt = conn.prepare("SELECT order_id, total, currency, status
@@ -190,12 +186,12 @@ fn main() {
                 Err(err) => panic!("error running query: {}", err)
             };
 
-            let row = match rows.next() {
+            let row = match rows.iter().next() {
                 Some(row) => row,
-                None => return Err(NickelError::new("No such order", ErrorWithStatusCode(NotFound)))
+                None => return Err(NickelError::new(response, "No such order", NotFound))
             };
 
-            let mut hal = pgsql_to_hal(stmt.result_descriptions(), &row);
+            let mut hal = pgsql_to_hal(stmt.columns(), &row);
 
             //let mut hal = Resource::new();
             //let descs = stmt.result_descriptions();
@@ -242,16 +238,14 @@ fn main() {
             //    .add_link("self", Link::new(format!("https://www.example.com/orders/{}", order_id).as_slice()));
 
             response
-                .content_type(mimes::Hal)
+                .content_type(mimes::MediaType::Hal)
                 .send(format!("{}", result));
-
-            Ok(Halt)
         }
 
         get "/setup" => |request, response| {
             let conn = connect();
 
-            conn.execute("DROP TABLE IF EXISTS orders", []).unwrap();
+            conn.execute("DROP TABLE IF EXISTS orders", &[]).unwrap();
 
             // todo: implement Numeric support
             conn.execute("CREATE TABLE orders (
@@ -259,7 +253,7 @@ fn main() {
                             total           DOUBLE PRECISION NOT NULL,
                             currency        VARCHAR NOT NULL,
                             status          VARCHAR NOT NULL
-                         )", []).unwrap();
+                         )", &[]).unwrap();
 
             conn.execute("INSERT INTO orders (order_id, total, currency, status)
                             VALUES ($1, $2, $3, $4)",
@@ -280,6 +274,10 @@ fn main() {
             response.send("Setup complete");
         }
     });
-    server.handle_error(error_handler);
-    server.listen(Ipv4Addr(127, 0, 0, 1), 6767);
+
+    // issue #20178
+    //let custom_handler: fn(&mut NickelError, &mut Request) -> Action = error_handler;
+    //server.handle_error(custom_handler);
+
+    server.listen("127.0.0.1:6767");
 }
